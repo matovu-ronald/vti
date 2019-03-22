c32e285d5445b98ccff0508e909ade2877f3e8b6
maron <maronwhite6@gmail.com>
2019-03-22 15:05:59 +0300

    Add bio profile crud route

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,4 +14,5 @@ Route::group([
     CRUD::resource('business', 'BusinessCrudController');
     CRUD::resource('vti', 'VtiCrudController');
     CRUD::resource('course', 'CourseCrudController');
+    CRUD::resource('bioprofile', 'BioProfileCrudController');
 }); // this should be the absolute last line of this file
