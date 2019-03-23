5f6479717a60ad2e06f1e3a812cc299c49531696
Matovu Ronald <maronwhite6@gmail.com>
2019-03-23 13:20:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -8,16 +8,16 @@ use Maatwebsite\Excel\Concerns\ToModel;
 class UsersImport implements ToModel
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return new User([
             'name'     => $row[0],
             'email'    => $row[1],
-            'password' => Hash::make($row[2])
+            'password' => Hash::make($row[2]),
         ]);
     }
 }
