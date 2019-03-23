28ccb9c491b1f7c803144ee61ef1d2ef3070d732
maron <maronwhite6@gmail.com>
2019-03-23 04:07:53 +0300

    Add FileUpload component and SampleExcel component

---
+++ resources/js/components/FileUploadComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div v-if="successUpload" class="callout callout-success">
+            <h4>Service Providers Imported Successfully</h4>
+        </div>
+        <multiple-file-uploader
+                postURL="http://127.0.0.1:8000/admin/import/service/providers"
+                successMessagePath=""
+                errorMessagePath=""
+                :minItems="1"
+                :maxItems="1"
+                dropAreaPrimaryMessage="Drop Excel file here"
+                @upload-success='success_handler'
+        >
+        </multiple-file-uploader>
+    </div>
+</template>
+
+<script>
+    import MultipleFileUploader from '@updivision/vue2-multi-uploader'
+
+    export default {
+        name: "FileUploadComponent",
+        components: {
+            MultipleFileUploader
+        },
+        data () {
+            return {
+                successUpload: false
+            }
+        },
+        methods: {
+            success_handler: function(response){
+                this.successUpload = true;
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/SampleExcelComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>Sample Excel File <a class="btn btn-primary btn-xs" href="#">Click to Download</a></h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SampleExcelComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
