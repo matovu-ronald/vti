a2e182a13d9a6b67916f5e99618963f3a96df9ea
maron <maronwhite6@gmail.com>
2019-03-25 07:46:44 +0300

    Add config file for the vti application

---
+++ config/vti.php
@@ -0,0 +1,10 @@
+<?php
+
+    return [
+        'imports' => [
+            'directory' => [
+                'courses' => 'imports/courses',
+                'providers' => 'imports/providers'
+            ]
+        ]
+    ];
