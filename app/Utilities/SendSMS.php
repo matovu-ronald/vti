45acbdfa6860b51813fe3ec9720af1889af7be0e
maron <maronwhite6@gmail.com>
2019-03-25 07:45:57 +0300

    Add SendSMS utility class for Africastalking api

---
+++ app/Utilities/SendSMS.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Utilities;
+
+use AfricasTalking\SDK\AfricasTalking;
+
+class SendSMS
+{
+    private $username;
+    private $apiKey;
+
+    public function __construct($username, $apiKey)
+    {
+        $this->username = $username;
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * Send sms
+     *
+     * @param $recepients
+     * @param $sentMessage
+     * @return array
+     */
+    public function sendSms($recipients, $message, $from)
+    {
+        $AT = new AfricasTalking($this->username, $this->apiKey);
+
+        // Get one of the services
+        $sms      = $AT->sms();
+
+        try {
+            // Thats it, hit send and we'll take care of the rest
+            $result = $sms->send([
+                'to'      => $recipients,
+                'message' => $message,
+                'from'    => $from
+            ]);
+
+            print_r($result);
+        } catch (Exception $e) {
+            echo "Error: ".$e->getMessage();
+        }
+
+        return $result;
+    }
+}
