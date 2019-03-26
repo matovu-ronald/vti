9f7385ad00568ad96f66e25ede98f48ad03b6de0
maron <maronwhite6@gmail.com>
2019-03-26 03:24:35 +0300

    Create VtiTableSeeder.php

---
+++ database/seeds/VtiTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+// composer require laracasts/testdummy
+use Laracasts\TestDummy\Factory as TestDummy;
+
+class VtiTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // TestDummy::times(20)->create('App\Post');
+        // Vtis
+        $vtis = factory(App\Models\Vti::class, 15)->create();
+
+        // Courses
+        $vtis->each(function ($vti) {
+            factory('App\Models\Course', 40)->create(['vti_id' => $vti->id]);
+        });
+    }
+}
