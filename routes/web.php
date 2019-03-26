f118168fa987ab2d2d839f96b8e4a9efee79e9fb
maron <maronwhite6@gmail.com>
2019-03-26 05:04:57 +0300

    Change import routes to web.php file to access logged in user data

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,13 @@ Route::get('/', function () {
     return redirect('/vocational-training-institute');
 });
 
+
+// Imports
+Route::prefix('api')->group(function () {
+    Route::post('import/service/providers', 'Admin\ImportServiceProviderController@store')->name('import.store');
+    Route::post('import/courses', 'Admin\ImportCourseController@store')->name('courses.store');
+});
+
 /* CATCH-ALL ROUTE for Backpack/PageManager - needs to be at the end of your routes.php file  **/
 Route::get('{page}/{subs?}', ['uses' => 'PageController@index'])
     ->where(['page' => '^((?!admin).)*$', 'subs' => '.*']);
