d4f703850c96e91fa7de1044a540dbc374370ba8
maron <maronwhite6@gmail.com>
2019-03-26 03:25:06 +0300

    Update ImportCourseController.php

--- app/Http/Controllers/Admin/ImportCourseController.php
+++ app/Http/Controllers/Admin/ImportCourseController.php
@@ -18,16 +18,6 @@ class ImportCourseController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
@@ -60,7 +50,17 @@ class ImportCourseController extends Controller
                 $successUploaded = $item->move($destination, $fileName);
 
                 if ($successUploaded) {
-                    Excel::import(new CoursesImport, $successUploaded);
+                    $courses = Excel::toCollection(new CoursesImport, $successUploaded);
+                    foreach ($courses[0] as $course) {
+                        Course::where('name', $course[1])->updateOrCreate([
+                            'vti_id' => $course[0],
+                            'name' => $course[1],
+                            'description' => $course[2]
+                        ]);
+
+
+                    }
+                    //\Log::info(\Auth::user()->name);
                     return response()->json(['message' => 'Courses imported successfully']);
                 }
 
@@ -92,51 +92,6 @@ class ImportCourseController extends Controller
             ]);
         }
 
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 
     /**
