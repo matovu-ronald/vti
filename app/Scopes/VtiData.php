65cfc26428a0b40b827d1f6b063ecb4545d31c84
maron <maronwhite6@gmail.com>
2019-03-28 19:00:30 +0300

    Add jobs migrations

---
+++ app/Scopes/VtiData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class VtiData implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $builder
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        if (!backpack_user()->hasRole('admin')) {
+
+            $builder->where('vti_id', backpack_user()->vti_id);
+        }
+    }
+}
