7a189b3bf8a6b7407bf7e8c4d81ca4391a78d2e5
maron <maronwhite6@gmail.com>
2019-03-28 19:05:37 +0300

    Fix error thrown when admin doesnot have default vti attached

--- resources/views/vendor/backpack/base/inc/main_header.blade.php
+++ resources/views/vendor/backpack/base/inc/main_header.blade.php
@@ -2,9 +2,13 @@
   <!-- Logo -->
   <a href="{{ url('/') }}" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini">{!! mini_logo(backpack_auth()->user()->vti->name) !!}</span>
+    <span class="logo-mini">
+      {!! backpack_user()->hasRole('admin') ?  config('backpack.base.logo_mini') : mini_logo(backpack_auth()->user()->vti->name) !!}
+    </span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg" style="font-size: 15px;">{!! backpack_auth()->user()->vti->name !!}</span>
+    <span class="logo-lg" style="font-size: 15px;">
+      {!! backpack_user()->hasRole('admin') ? config('backpack.base.logo_lg') : backpack_auth()->user()->vti->name !!}
+    </span>
   </a>
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top" role="navigation">
