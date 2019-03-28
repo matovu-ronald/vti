0aeb043ea9f679bca5a30941115a873c633e6818
maron <maronwhite6@gmail.com>
2019-03-28 19:09:24 +0300

    Add links, ata, profile_pic keys

--- config/vti.php
+++ config/vti.php
@@ -7,4 +7,14 @@
                 'providers' => 'imports/providers',
             ],
         ],
+        "links" => [
+            'login' => 'http://127.0.0.1:8000/vocational-training-institute/'
+        ],
+        'ata' => [
+            //'api' => '74cacbd6faa71286a00131eb4f04e2d2864509e93078fb33088905fafbf49ee4',
+            'api' => 'd5319c3a6028613d0eb10d4a03e35ed535ce71eee1a6b1c8112907ada7671b0e',
+            'username' => 'sandbox'
+        ],
+
+        'profile_pic' => 'user.png'
     ];
