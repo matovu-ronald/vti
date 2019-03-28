3fd1b9920eac13046d42ca00cb87cb1c535ceca8
maron <maronwhite6@gmail.com>
2019-03-28 19:10:59 +0300

    Add service provider import functionality

--- app/Http/Controllers/Admin/ImportServiceProviderController.php
+++ app/Http/Controllers/Admin/ImportServiceProviderController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Events\ServiceProviderCreated;
 use App\Http\Controllers\Controller;
 use App\Imports\UsersImport;
+use App\Models\BackpackUser;
 use App\Models\BioProfile;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Facades\Excel;
+use \Cviebrock\EloquentSluggable\Services\SlugService;
 
 class ImportServiceProviderController extends Controller
 {
     protected $uploadPath;
+    private $userEmail = '';
+    private $userPassword = '';
 
     public function __construct()
     {
@@ -41,25 +46,59 @@ class ImportServiceProviderController extends Controller
                 $successUploaded = $item->move($destination, $fileName);
 
                 if ($successUploaded) {
+                    config(['excel.import.startRow' => 2]);
                     $users = Excel::toCollection(new UsersImport(), $successUploaded);
                     foreach ($users[0] as $user) {
 
-                        $userData = User::where('email', $user[1])->updateOrCreate(['email' => $user[1]], [
-                            'vti_id' => backpack_auth()->user()->vti->id,
-                            'name' => $user[0],
-                            'email' => $user[1],
-                            'email_verified_at' => Carbon::now(),
-                            'password' => $this->randomPassword(),
-                        ]);
 
-                        $bioProfile = BioProfile::where('phone', $user[2])->updateOrCreate(['phone' => $user[2]], [
-                            'user_id' => $userData->id,
-                            'phone' => $user[2],
-                            'address' => $user[3],
-                            'course' => $user[4]
-                        ]);
 
-                        \Log::info($userData);
+                        if (empty($user[1])) {
+                            $this->userEmail = $this->generateUniqueEmail($user[0]);
+                        }else {
+                            $this->userEmail = $user[1];
+                        }
+                        // Check if a bio profile with a certain phone number already exists
+                        $bioData = BioProfile::where('phone', $user[2])->first();
+
+                        // If the bio profile with the phone number already exists
+                        // Update the user data associated with that bio profile
+                        // Donot create a new user
+                        if ($bioData) {
+                            $this->userPassword = $this->randomPassword();
+                            $userData = User::where('id', $bioData->user_id)->update([
+                                'vti_id' => backpack_auth()->user()->vti->id,
+                                'name' => $user[0],
+                                //'email' => $this->userEmail,
+                                //'password' => bcrypt($this->userPassword),
+                            ]);
+
+                            //$this->assignUserRoles($userData);
+
+                            // event(new ServiceProviderCreated($userData, $this->userPassword));
+
+                        }else {
+                            $this->userPassword = $this->randomPassword();
+                            $userData = User::where('email', $user[1])->updateOrCreate(['email' => $user[1]], [
+                                'vti_id' => backpack_auth()->user()->vti->id,
+                                'name' => $user[0],
+                                'email' => $this->userEmail,
+                                'email_verified_at' => Carbon::now(),
+                                'password' => bcrypt($this->userPassword),
+                            ]);
+
+                            $this->assignUserRoles($userData);
+
+                            $bioProfile = BioProfile::where('phone', $user[2])->updateOrCreate(['phone' => $user[2]], [
+                                'user_id' => $userData->id,
+                                'phone' => $user[2],
+                                'address' => $user[3],
+                                'course' => $user[4]
+                            ]);
+
+                            event(new ServiceProviderCreated($userData, $this->userPassword, $bioProfile));
+                        }
+
+
                     }
 
                     \Alert::success($users->count().'imported successfully');
@@ -91,8 +130,38 @@ class ImportServiceProviderController extends Controller
         return $validator;
     }
 
+    /**
+     * Generate random password
+     *
+     * @return string
+     */
     private function randomPassword()
     {
-        return bcrypt(Str::random(8));
+        return Str::random(8);
+    }
+
+    /**
+     * Assign user roles
+     *
+     * @param $userData
+     */
+    private function assignUserRoles($userData)
+    {
+        $userData->roles()->detach();
+
+        $roles = ['public', 'provider'];
+
+        $userData->assignRole($roles);
+    }
+
+    /**
+     * Generate unique Email
+     *
+     * @param $username
+     * @return string
+     */
+    private function generateUniqueEmail($username) {
+        $sluggedUsername = SlugService::createSlug(User::class, 'email', $username);
+        return $sluggedUsername . mt_rand(2, 999) . "@" .strtolower(mini_logo(backpack_auth()->user()->vti->name)). ".com";
     }
 }
